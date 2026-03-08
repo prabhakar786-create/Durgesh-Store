585c67e0d317b6d29f57f46f4523718198f3c0cd
prabhakar786-create <prabhakar786@gmail.com>
2026-03-08 17:46:32 +0530

    Revise product data with new items and images
    Updated product list with additional items and corrected image paths.

--- products.js
+++ products.js
@@ -1,39 +1,42 @@
-﻿const products = [
+const products = [
 
-{
-id:1,
-name:"Aata 5kg",
-price:250,
-image:"aata.jpg"
-},
+{ id:1, name:"Aata 5kg", price:250, image:"images/aata.jpg" },
+{ id:2, name:"Chawal 5kg", price:300, image:"images/chawal.jpg" },
+{ id:3, name:"Mustard Oil 1L", price:180, image:"images/oil.jpg" },
+{ id:4, name:"Sugar 1kg", price:45, image:"images/sugar.jpg" },
+{ id:5, name:"Salt 1kg", price:20, image:"images/salt.jpg" },
+{ id:6, name:"Tur Dal 1kg", price:120, image:"images/dal.jpg" },
+{ id:7, name:"Chana Dal 1kg", price:95, image:"images/chana.jpg" },
+{ id:8, name:"Moong Dal 1kg", price:130, image:"images/moong.jpg" },
+{ id:9, name:"Tea 250g", price:140, image:"images/tea.jpg" },
+{ id:10, name:"Coffee 100g", price:120, image:"images/coffee.jpg" },
 
-{
-id:2,
-name:"Chawal 5kg",
-price:300,
-image:"chawal.jpg"
-},
+{ id:11, name:"Parle Biscuit", price:10, image:"images/biscuit.jpg" },
+{ id:12, name:"Marie Biscuit", price:25, image:"images/marie.jpg" },
+{ id:13, name:"Good Day Biscuit", price:30, image:"images/goodday.jpg" },
 
-{
-id:3,
-name:"Mustard Oil",
-price:180,
-image:"oil.jpg"
-},
+{ id:14, name:"Maggi Noodles", price:14, image:"images/maggi.jpg" },
+{ id:15, name:"Pasta", price:70, image:"images/pasta.jpg" },
 
-{
-id:4,
-name:"Parle Biscuit",
-price:10,
-image:"biscuit.jpg"
-},
+{ id:16, name:"Lux Soap", price:35, image:"images/soap.jpg" },
+{ id:17, name:"Lifebuoy Soap", price:30, image:"images/lifebuoy.jpg" },
+{ id:18, name:"Dove Soap", price:65, image:"images/dove.jpg" },
 
-{
-id:5,
-name:"Lux Soap",
-price:35,
-image:"soap.jpg"
-}
+{ id:19, name:"Surf Excel", price:110, image:"images/surf.jpg" },
+{ id:20, name:"Washing Powder", price:90, image:"images/detergent.jpg" },
+
+{ id:21, name:"Shampoo Sachet", price:2, image:"images/shampoo.jpg" },
+{ id:22, name:"Hair Oil", price:80, image:"images/hairoil.jpg" },
+
+{ id:23, name:"Toothpaste", price:95, image:"images/toothpaste.jpg" },
+{ id:24, name:"Toothbrush", price:25, image:"images/toothbrush.jpg" },
+
+{ id:25, name:"Milk Packet", price:60, image:"images/milk.jpg" },
+{ id:26, name:"Paneer 200g", price:90, image:"images/paneer.jpg" },
+{ id:27, name:"Butter 100g", price:55, image:"images/butter.jpg" },
+
+{ id:28, name:"Potato 1kg", price:25, image:"images/potato.jpg" },
+{ id:29, name:"Onion 1kg", price:30, image:"images/onion.jpg" },
+{ id:30, name:"Tomato 1kg", price:35, image:"images/tomato.jpg" }
 
 ];
-
