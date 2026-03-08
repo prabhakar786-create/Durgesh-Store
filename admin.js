00d784d5a5769057b68f2cb674afd5a31f3158e3
prabhakar786-create <prabhakar786@gmail.com>
2026-03-08 18:04:21 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,29 @@
+﻿let products = JSON.parse(localStorage.getItem("products")) || [];
+
+function addProduct(){
+
+let name = document.getElementById("name").value;
+
+let price = document.getElementById("price").value;
+
+let image = document.getElementById("image").value;
+
+let product = {
+
+id: Date.now(),
+
+name: name,
+
+price: price,
+
+image: image
+
+};
+
+products.push(product);
+
+localStorage.setItem("products", JSON.stringify(products));
+
+alert("Product Added");
+
+}
