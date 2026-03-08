9455b5b0eca31d1e41a6956ac9d940d026fba348
prabhakar786-create <prabhakar786@gmail.com>
2026-03-08 16:28:35 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,33 @@
+﻿function sendOrder(){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let name = document.getElementById("name").value;
+
+let phone = document.getElementById("phone").value;
+
+let address = document.getElementById("address").value;
+
+let text = "New Order:%0A";
+
+text += "Name: " + name + "%0A";
+
+text += "Phone: " + phone + "%0A";
+
+text += "Address: " + address + "%0A";
+
+text += "Items:%0A";
+
+cart.forEach(i => {
+
+text += i.name + " ₹" + i.price + "%0A";
+
+});
+
+let store = "91XXXXXXXXXX";
+
+let url = "https://wa.me/" + store + "?text=" + text;
+
+window.open(url);
+
+}
---
+++ products.js
@@ -0,0 +1,38 @@
+﻿const products = [
+
+{
+id:1,
+name:"Aata 5kg",
+price:250,
+image:"images/aata.jpg"
+},
+
+{
+id:2,
+name:"Chawal 5kg",
+price:300,
+image:"images/chawal.jpg"
+},
+
+{
+id:3,
+name:"Mustard Oil",
+price:180,
+image:"images/oil.jpg"
+},
+
+{
+id:4,
+name:"Parle Biscuit",
+price:10,
+image:"images/biscuit.jpg"
+},
+
+{
+id:5,
+name:"Lux Soap",
+price:35,
+image:"images/soap.jpg"
+}
+
+];
