fa6255acf2f6ac7328601a34dd63a15e64bb73f8
prabhakar786-create <prabhakar786@gmail.com>
2026-03-08 16:34:09 +0530

    Change image paths to relative URLs in products.js
    Updated image paths to relative URLs.

--- products.js
+++ products.js
@@ -4,35 +4,36 @@
 id:1,
 name:"Aata 5kg",
 price:250,
-image:"images/aata.jpg"
+image:"aata.jpg"
 },
 
 {
 id:2,
 name:"Chawal 5kg",
 price:300,
-image:"images/chawal.jpg"
+image:"chawal.jpg"
 },
 
 {
 id:3,
 name:"Mustard Oil",
 price:180,
-image:"images/oil.jpg"
+image:"oil.jpg"
 },
 
 {
 id:4,
 name:"Parle Biscuit",
 price:10,
-image:"images/biscuit.jpg"
+image:"biscuit.jpg"
 },
 
 {
 id:5,
 name:"Lux Soap",
 price:35,
-image:"images/soap.jpg"
+image:"soap.jpg"
 }
 
 ];
+
